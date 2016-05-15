5d0efbc06d98e0a2c46465c9b48423409a85b8a4
ddshah001 <ddshah001@gmail.com>
2016-05-15 21:26:30 +0530

    function and header added

---
+++ theme/searchform.php
@@ -0,0 +1,6 @@
+<div class="searchbar">
+	<form name="search" method="get" action"/">
+			<input type="text" name="s" value="Search…" />
+			<input type="submit" name="submit" value="Search!" />
+	</form>
+</div>
