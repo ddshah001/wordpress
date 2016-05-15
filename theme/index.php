9e71f8d70840d104413a4a948441073691e67c80
ddshah001 <ddshah001@gmail.com>
2016-05-15 23:04:44 +0530

    footer added

---
+++ theme/index.php
@@ -0,0 +1,51 @@
+<?php get_header()?>
+
+<div id="main" class="group">
+					<div id="blog" class="left-col">
+						<div class="post">
+							<h2>Morbi leo risus, porta ac consectetur</h2>
+							<div class="byline">by <a href="#">HAROLD OVERMAN</a> on <a href="#">THURSDAY, JANUARY 12, 2012</a></div>
+
+							<p>Ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare el eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+
+							<p>Ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare el eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+						</div>
+
+						<div class="post">
+							<h2>Morbi leo risus, porta ac consectetur</h2>
+							<div class="byline">by <a href="#">HAROLD OVERMAN</a> on <a href="#">THURSDAY, JANUARY 12, 2012</a></div>
+
+							<p>Ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Nullam quis risus eget urna mollis ornare el eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+						</div>
+
+						<div class="navi">
+							<div class="right">
+								<a href="#">Previous</a> / <a href="#">Next</a>
+							</div>
+
+							<ul class="pages">
+								<li>Page</li>
+								<li><a href="#">1</a></li>
+								<li>2</li>
+								<li><a href="#">3</a></li>
+								<li><a href="#">4</a></li>
+								<li><a href="#">5</a></li>
+								<li><a href="#">6</a></li>
+								<li><a href="#">7</a></li>
+							</ul>
+						</div>
+
+					</div>
+					<aside class="right-col">
+						<div class="widget">
+							<h3>ABOUT US</h3>
+							<p>Ac, vestibulum at eros. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Maecenas faucibus mollis interdum. Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+
+							<p>Vestibulum id ligula porta felis euismod semper. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et.</p>
+
+							<p><a href="#">MORE ABOUT US...</a></p>
+						</div>
+					</aside>
+				</div>
+
+<?php get_footer()?>
